a113294e3670ba9109cbd043491917aed33ebc1a
David Benicek <david.benicek@skyscanner.net>
2018-10-30 08:40:07 +0100

    Nasty hack to get it to deploy now

--- server.js
+++ server.js
@@ -2,12 +2,16 @@
 //Lib imports
 var express = require('express');
 var bodyParser = require('body-parser');
+const cmd = require('node-cmd');
 
 const vocab = require('./services/vocab');
 
 //Start server
 var app = express();
 var port = Number(process.env.BACKEND_PORT || 1200);
+
+cmd.run('cd client && npm start');
+
 
 app.use(bodyParser.json({limit: '10mb'}));
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
